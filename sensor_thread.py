dca050318701dbd38d11e702ede70d55a1579b0a
stella-jo <0604seohee@gmail.com>
2025-07-16 19:55:30 +0900

    Update linear regression

--- sensor_thread.py
+++ sensor_thread.py
@@ -2,6 +2,8 @@ import minimalmodbus
 import serial
 import RPi.GPIO as GPIO
 import time
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 RELAY_PIN = 17
 
@@ -13,6 +15,8 @@ instrument.serial.stopbits = 1
 instrument.serial.timeout = 1
 instrument.mode = minimalmodbus.MODE_RTU
 
+power_buffer = []
+buffer_size = 30
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
@@ -39,6 +43,18 @@ def read_energy():
     raw = (regs[1] << 16) + regs[0]
     return raw * 1.0
 
+def predict_power(buffer):
+    if len(buffer) < 30:
+        return buffer[-1] if buffer else 0.0
+    
+    X = np.arange(len(buffer)).reshape(-1, 1)
+    y = np.array(buffer).reshape(-1, 1)
+    model = LinearRegression()
+    model.fit(X, y)
+    future_x = np.array([[len(buffer) + 30]])
+    prediction = model.predict(future_x)[0][0]
+    return float(prediction)
+
 def sensor_loop(socketio):
     while True:
         try: 
@@ -46,9 +62,16 @@ def sensor_loop(socketio):
             power = read_power()
             energy = read_energy()
 
+            power_buffer.append(power)
+            if len(power_buffer) > buffer_size:
+                power_buffer.pop(0)
+
+            pred_power = predict_power(power_buffer)
+
             values = {
                 'current': round(current, 3),
                 'power': round(power, 1),
+                'predicted_power': round(pred_power, 1),
                 'energy': round(energy, 2),
             }
 
