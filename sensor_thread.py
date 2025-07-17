60a1488767bba695ecb45c381c03f2ddbd78a8ce
stella-jo <0604seohee@gmail.com>
2025-07-17 13:08:04 +0900

    Delete prediction

--- sensor_thread.py
+++ sensor_thread.py
@@ -15,9 +15,6 @@ instrument.serial.stopbits = 1
 instrument.serial.timeout = 1
 instrument.mode = minimalmodbus.MODE_RTU
 
-power_buffer = []
-buffer_size = 30
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 
@@ -43,18 +40,6 @@ def read_energy():
     raw = (regs[1] << 16) + regs[0]
     return raw * 1.0
 
-def predict_power(buffer):
-    if len(buffer) < 30:
-        return buffer[-1] if buffer else 0.0
-    
-    X = np.arange(len(buffer)).reshape(-1, 1)
-    y = np.array(buffer).reshape(-1, 1)
-    model = LinearRegression()
-    model.fit(X, y)
-    future_x = np.array([[len(buffer) + 30]])
-    prediction = model.predict(future_x)[0][0]
-    return float(prediction)
-
 def sensor_loop(socketio):
     while True:
         try: 
@@ -62,16 +47,9 @@ def sensor_loop(socketio):
             power = read_power()
             voltage = read_voltage()
 
-            power_buffer.append(power)
-            if len(power_buffer) > buffer_size:
-                power_buffer.pop(0)
-
-            pred_power = predict_power(power_buffer)
-
             values = {
                 'current': round(current, 3),
                 'power': round(power, 1),
-                'predicted_power': round(pred_power, 1),
                 'voltage': round(voltage, 1),
             }
 
